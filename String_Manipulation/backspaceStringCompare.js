c36bcbfe030b7197d1c88db8e9da72f5750a16c1
Kevin Heleodoro <kevin.heleodoro@gmail.com>
2022-05-05 12:43:04 -0500

    leetcode #844

---
+++ String_Manipulation/backspaceStringCompare.js
@@ -0,0 +1,48 @@
+// LeetCode #844
+
+// ### Instructions
+// - Given two Strings 'S' and 'T', return if they equal when both are types out. Any '#' that appears in the string counts as a backspace.
+// >- Example: 'cb#d === 'cd'
+
+// // best case tests
+
+// const S = 'ab#z';
+// const T = 'az#z';
+
+// // both S and T will equal 'az'
+// // returns true
+
+// const S = 'abc#d'; // abd
+// const T = 'acc#c'; // acc
+// // returns false
+
+// const S = 'x#y#z#'; // ''
+// const T = 'a#'; // ''
+// // returns true
+
+// const S = 'a###b'; // b
+// const T = 'b'; // b
+// //returns true
+
+const S = 'Ab#z'; // 'Az'
+const T = 'ab#z'; // 'az'
+// // returnd false
+
+function equalityTester(S, T) {
+	return hashChecker(S) === hashChecker(T);
+}
+
+function hashChecker(str) {
+	let result = [];
+	for (let i = 0; i < str.length; i++) {
+		if (str[i] === '#' || str[i + 1] === '#') {
+			continue;
+		} else {
+			result.push(str[i]);
+		}
+	}
+
+	return result.join('');
+}
+
+console.log(equalityTester(S, T));
